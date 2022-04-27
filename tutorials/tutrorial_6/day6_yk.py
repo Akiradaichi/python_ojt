d0967d03cfa2b4f3b422acbb2b1c910abdbcacf3
Akiradaichi <shirayu7ki@gmail.com>
2022-04-28 01:17:14 +0630

    # 6 update some error

--- tutorials/tutrorial_6/day6_yk.py
+++ tutorials/tutrorial_6/day6_yk.py
@@ -3,31 +3,26 @@ class Person:
     def __init__(self, fname, lname):
         self.fname = fname
         self.lname = lname
-
     def myfunc(self):
         print("a first name of employee is " + self.fname)
-
-
 p1 = Person("John", "Nathan")
 p1.myfunc()
-
-
 class Employee(Person):
     def __init__(self, fname, lname):
         super().__init__(fname, lname)
-
-
-p2 = Employee("Authur", "King")
-p2.myfunc()
+emp = Employee("Authur", "King")
+emp.myfunc()
 
 # 2
-
 l = list(range(1, 11))
-
-
 iter_l = l.__iter__()
-while True:
-    try:
-        print(iter_l.__next__())
-    except:
-        raise StopIteration
+print(iter_l.__next__())
+print(iter_l.__next__())
+print(iter_l.__next__())
+print(iter_l.__next__())
+print(iter_l.__next__())
+print(iter_l.__next__())
+print(iter_l.__next__())
+print(iter_l.__next__())
+print(iter_l.__next__())
+print(iter_l.__next__())
