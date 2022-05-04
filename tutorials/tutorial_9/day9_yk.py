c3278c745486a9d8f84e28ef19b5d3ea399578b7
Akiradaichi <shirayu7ki@gmail.com>
2022-05-04 10:07:00 +0630

    # day 11 update

--- tutorials/tutorial_9/day9_yk.py
+++ tutorials/tutorial_9/day9_yk.py
@@ -10,8 +10,8 @@ while z > 0:
 6.modulo
 7.square-root
 8.cube""")
-
     user_input = str(input("Enter choice:"))
+    print(" ")
 
     def add_operation(first, second):
         x = first + second
@@ -49,75 +49,147 @@ while z > 0:
         while True:
             try:
                 first = float(input("first number:"))
-                second = float(input("second number:"))
+                print(" ")
+                while True:
+                    try:
+                        second = float(input("second number:"))
+                        print(" ")
+                        break
+                    except ValueError:
+                        print(" ")
+                        print("Second Number has to be a number !!")
+                        print(" ")
                 add_operation(first, second)
                 break
             except ValueError:
-                print("Valid number pls")
+                print(" ")
+                print("First number has to be a number !!")
+                print(" ")
     elif user_input.lower() == "subtract" or user_input == "2":
         while True:
             try:
                 first = float(input("first number:"))
-                second = float(input("second number:"))
+                print(" ")
+                while True:
+                    try:
+                        second = float(input("second number:"))
+                        print(" ")
+                        break
+                    except ValueError:
+                        print(" ")
+                        print("Second Number has to be a number !!")
+                        print(" ")
                 subtract_operation(first, second)
                 break
             except ValueError:
-                print("Valid number pls")
+                print(" ")
+                print("First number has to be a number !!")
+                print(" ")
     elif user_input.lower() == "multiply" or user_input == "3":
         while True:
             try:
                 first = float(input("first number:"))
-                second = float(input("second number:"))
+                print(" ")
+                while True:
+                    try:
+                        second = float(input("second number:"))
+                        print(" ")
+                        break
+                    except ValueError:
+                        print(" ")
+                        print("Second Number has to be a number !!")
+                        print(" ")
                 multiply_operation(first, second)
                 break
             except ValueError:
-                print("Valid number pls")
+                print(" ")
+                print("First number has to be a number !!")
+                print(" ")
     elif user_input.lower() == "divide" or user_input == "4":
         while True:
             try:
                 first = float(input("first number:"))
-                second = float(input("second number:"))
+                print(" ")
+                while True:
+                    try:
+                        second = float(input("second number:"))
+                        print(" ")
+                        break
+                    except ValueError:
+                        print(" ")
+                        print("Second Number has to be a number !!")
+                        print(" ")
                 divide_operation(first, second)
                 break
             except ValueError:
-                print("Valid number pls")
+                print(" ")
+                print("First number has to be a number !!")
+                print(" ")
     elif user_input.lower() == "power" or user_input == "5":
         while True:
             try:
                 first = float(input("first number:"))
-                second = float(input("second number:"))
+                print(" ")
+                while True:
+                    try:
+                        second = float(input("second number:"))
+                        print(" ")
+                        break
+                    except ValueError:
+                        print(" ")
+                        print("Second Number has to be a number !!")
+                        print(" ")
                 power_operation(first, second)
                 break
             except ValueError:
-                print("Valid number pls")
+                print(" ")
+                print("First number has to be a number !!")
+                print(" ")
     elif user_input.lower() == "modulo" or user_input == "6":
         while True:
             try:
                 first = float(input("first number:"))
-                second = float(input("second number:"))
+                print(" ")
+                while True:
+                    try:
+                        second = float(input("second number:"))
+                        print(" ")
+                        break
+                    except ValueError:
+                        print(" ")
+                        print("Second Number has to be a number !!")
+                        print(" ")
                 modulo_operation(first, second)
                 break
             except ValueError:
-                print("Valid number pls")
+                print(" ")
+                print("First number has to be a number !!")
+                print(" ")
     elif user_input.lower() == "square root" or user_input == "7":
         while True:
             try:
                 number = float(input("enter number :"))
+                print(" ")
                 squareroot_operation(number)
                 break
             except ValueError:
-                print("Valid number pls")
+                print(" ")
+                print("Number has to be a number !!")
+                print(" ")
     elif user_input.lower() == "cube" or user_input == "8":
         while True:
             try:
                 number = float(input("enter number :"))
+                print(" ")
                 cube_operation(number)
                 break
             except ValueError:
-                print("Valid number pls")
+                print(" ")
+                print("Number has to be a number !!")
+                print(" ")
     else:
         print("invalid input")
-
+    print(" ")
     user_again = str(input("Let's do next calculation? (yes/no)"))
 
     if user_again.lower() == "yes":
